171da3641da234325eb9233bb3cbce8a790903c3
NadezhdaSeregovna <demidchik1307@yandex.ru>
2025-01-17 21:12:32 +0800

    Update search_notes_function.py

--- search_notes_function.py
+++ search_notes_function.py
@@ -50,36 +50,35 @@ def search_notes(notes, keyword=None, status=None):
     return notes_search
 
 
-
-# Список заметок
-# notes = []
-notes = [
-    {'username': 'Алексей', 'title': 'Список покупок', 'content': 'Купить продукты на неделю',
-     'status': 'новая', 'created_date': '27-11-2024', 'issue_date': '30-11-2024'},
-    {'username': 'Мария', 'title': 'Учеба', 'content': 'Подготовиться к экзамену',
-     'status': 'в процессе', 'created_date': '25-11-2024', 'issue_date': '01-12-2024'},
-    {'username': 'Иван', 'title': 'План работы', 'content': 'Завершить проект',
-     'status': 'выполнено', 'created_date': '20-11-2024', 'issue_date': '26-11-2024'}
-]
-# запуск поиска
-while True:
-    print('Выберите тип поиска  \n1 - поиск по ключевому слову '
-          '\n2 - поиск по статусу \n3 - поиск по ключевому полю и статусу: ')
-    ans = input('Введите цифру: ')
-    if ans == '1':
-        keyword = input('Введите имя, название или описание заметки для поиска: ')
-        keyword = search_notes(notes, keyword)
-        break
-    elif ans == '2':
-        status = input('Введите статус заметки для поиска (новая, в процессе, выполнено: ')
-        status = search_notes(notes, status)
-        break
-    elif ans == '3':
-        keyword = input('Введите имя, название или описание заметки для поиска: ')
-        status = input('Введите статус заметки для поиска: ')
-        notes = search_notes(notes, keyword, status)
-        break
-    else:
-        print('Ошибка ввода.')
-        continue
-
+if __name__ == "__main__":
+    # Список заметок
+    # notes = []
+    notes = [
+        {'username': 'Алексей', 'title': 'Список покупок', 'content': 'Купить продукты на неделю',
+         'status': 'новая', 'created_date': '27-11-2024', 'issue_date': '30-11-2024'},
+        {'username': 'Мария', 'title': 'Учеба', 'content': 'Подготовиться к экзамену',
+         'status': 'в процессе', 'created_date': '25-11-2024', 'issue_date': '01-12-2024'},
+        {'username': 'Иван', 'title': 'План работы', 'content': 'Завершить проект',
+         'status': 'выполнено', 'created_date': '20-11-2024', 'issue_date': '26-11-2024'}
+    ]
+    # запуск поиска
+    while True:
+        print('Выберите тип поиска  \n1 - поиск по ключевому слову '
+              '\n2 - поиск по статусу \n3 - поиск по ключевому полю и статусу: ')
+        ans = input('Введите цифру: ')
+        if ans == '1':
+            keyword = input('Введите имя, название или описание заметки для поиска: ')
+            keyword = search_notes(notes, keyword)
+            break
+        elif ans == '2':
+            status = input('Введите статус заметки для поиска (новая, в процессе, выполнено: ')
+            status = search_notes(notes=notes, keyword=None, status=status)
+            break
+        elif ans == '3':
+            keyword = input('Введите имя, название или описание заметки для поиска: ')
+            status = input('Введите статус заметки для поиска: ')
+            notes = search_notes(notes, keyword, status)
+            break
+        else:
+            print('Ошибка ввода.')
+            continue
