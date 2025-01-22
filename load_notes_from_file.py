b066179a56f67093401ad6c2a8a2b20c271b5faf
NadezhdaSeregovna <demidchik1307@yandex.ru>
2025-01-23 01:06:27 +0800

    Update load_notes_from_file.py

--- load_notes_from_file.py
+++ load_notes_from_file.py
@@ -3,37 +3,40 @@
 def load_notes_from_file(filename):
     try:
        with open(filename, 'r', encoding='utf-8') as file:  # Открыть файл в режиме чтения
-            info = file.readlines() # чтение строк в файле
+        info = file.read() # чтение строк в файле
+        # print(info)
+        notes = []  # соднание списка для хранения словарей
+        if info:
+            note_blocks = info.split('---')  # разделение текста из файла на заметки по разделителю
+            # print(note_blocks)
+            for block in note_blocks:
+                note_lines = block.strip().split('\n')
+                note_lines = list(filter(None, note_lines))#удаление пустых строк
+                # print(note_lines)
+                note = {}  # соднание словаря для хранения заметки
+
+                for line in note_lines:
+                    #разбиение строки на ключ и значение
+                    key, value = line.split(': ', 1)
+                    note[key] = value
+                notes.append(note)
+            # print(notes)
+
     except FileNotFoundError: #отработка исключения, если файла не существует
-        print('Файл не найден')
+        print(f'Файл {filename} не найден')
         return []
 
-    #удаление лишних символов, разделитей заметок
-    symbols = "---"
-    info= [line for line in info if line != symbols]
+    except Exception as e:
+        print(f"Произошла ошибка при чтении файла: {e}")
 
-    notes = []  # соднание списка для хранения словарей
-
-    #получение пары ключ - значение для словаря
-    try:
-        for line in info:
-            note = {}  # соднание словаря для хранения заметки
-            key, value = line.split(': ')
-            note[key] = value
-        notes.append(note)
-    except:
-        print("Проверьте содержимое файла")
-
-
-
-    #вывод полученных заметок
-    for i, note in (notes):
-        print(f"Имя пользователя: {note['username']} \nЗаголовок: {note['title']} "
-              f"\nОписание: {note['content']} \nСтатус: {note['status']} "
-              f"\nДата создания: {note['created_date']} \nДедлайн: {note['issue_date']}")
     return notes
 
+
 if __name__ == "__main__":
-    filename = 'filename.txt'
-    load_notes_from_file(filename)
+    notes = load_notes_from_file('filename.txt')
+    for note in (notes):
+        print(note)
+
+
+  
 
